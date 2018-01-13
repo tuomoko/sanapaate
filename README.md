7452400bc62473924d9f3e943b1e951bababc132
Tuomo Kohtamäki <tuomo.kohtamaki@gmail.com>
2018-01-13 15:34:22 +0200

    Added possibility to use command line arguments

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 Etsii tietyn päätteisiä sanoja kotusin sanalistasta.
+
+Lataa ensin sanalista osoitteesta http://kaino.kotus.fi/sanat/nykysuomi/ Pura sanalista samaan paikkaan skriptin kanssa.
+
+Käyttö:
+```
+python parse.py kaali
+```
+hakee kaikki -kaali päätteiset sanat.
--- parse.py
+++ parse.py
@@ -1,17 +1,28 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+# By Tuomo Kohtamäki 13.1.2018
+
 import xml.etree.ElementTree
 import re
-
-ending = 'in'
+import argparse
 
 # Lataa sanalista osoitteesta
 # http://kaino.kotus.fi/sanat/nykysuomi/kotus-sanalista-v1.tar.gz
-lista = 'kotus-sanalista_v1.xml'
+wordlist = 'kotus-sanalista_v1.xml'
 
-e = xml.etree.ElementTree.parse(lista).getroot()
-pattern = re.escape(ending)
+parser = argparse.ArgumentParser(description='Etsi sanapäätteitä kotuksen listasta.')
+parser.add_argument('paate', type=str, help='etsittävä pääte')
+args = parser.parse_args()
 
-for record in e.findall('st'):
-    word = record.findall('s')[0].text
-    linematch = re.match(r'.*'+pattern+'$',word)
-    if linematch:
-        print(word)
+if args.paate:
+    ending = args.paate
+    e = xml.etree.ElementTree.parse(wordlist).getroot()
+    pattern = re.escape(ending)
+
+    for record in e.findall('st'):
+        word = record.findall('s')[0].text
+        linematch = re.match(r'.*'+pattern+'$',word)
+        if linematch:
+            print(word.encode('utf-8'))
+
